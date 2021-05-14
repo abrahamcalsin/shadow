fa48e068b93d21afa9f5bbda3ff6df8fa1c4ac4e
abrahamcalsin <abrahamcalsin.16@gmail.com>
2021-05-13 22:16:33 -0500

    initial commit

---
+++ Script/app.js
@@ -0,0 +1,37 @@
+function CopyFunction(item) {
+    var id = item.previousElementSibling.id
+
+    const copyId = document.getElementById(`${id}`);
+    const newCont = copyId.innerText;
+
+    const shadowCont = document.createElement('textarea');
+    shadowCont.value = newCont;
+    shadowCont.setAttribute('readonly', '');
+    document.body.appendChild(shadowCont);
+    shadowCont.select();
+    document.execCommand('copy');
+    document.body.removeChild(shadowCont);
+    
+    //Create Span
+    var newSpan = document.createElement("span");
+    newSpan.classList = "successCopy";
+    newSpan.innerText = '¡Copied!';
+
+    document.body.appendChild(newSpan);
+
+    //Style
+
+    copyId.classList.add("bgCopyId");
+    setTimeout(() => {
+        copyId.classList.remove("bgCopyId");
+
+        //Remove Span
+        document.body.removeChild(document.body.lastChild); 
+
+    }, 1000);
+    
+
+    //alert(`Copia Exitosa: ${copyText}`);
+    
+    //console.log(item.previousElementSibling.id);
+}
